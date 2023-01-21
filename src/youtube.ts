643ac0e2fd25ca96b284e9f2d95e0d35424020b2
m77so <m77code@gmail.com>
2023-01-21 16:46:13 +0900

    stream用にフィルタを除去　そのうち考える

--- src/youtube.ts
+++ src/youtube.ts
@@ -2,24 +2,28 @@ import ytdl from 'ytdl-core'
 import { Song, MusicSite } from './interface'
 import {URL} from 'url'
 const id = 'youtube'
-import {AudioPlayer, AudioResource, createAudioPlayer, createAudioResource,StreamType, VoiceConnection } from '@discordjs/voice';
+import { AudioResource,  createAudioResource,StreamType } from '@discordjs/voice';
 
 
 const resource = function(song: Song): AudioResource {
-
     const stream = ytdl(ytdl.getURLVideoID(song.url), {
-        filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus
+        // filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus
         quality: 'highest',
         highWaterMark: 32 * 1024 * 1024, // https://github.com/fent/node-ytdl-core/issues/902
-      });
+      }
+      );
     const resource = createAudioResource(stream, {
-        inputType: StreamType.WebmOpus
+        inputType: StreamType.Arbitrary,
+        inlineVolume: true
     });
     return resource
 }
 
 const getInfo = async function (url: string): Promise<Song> {
     const songInfo = await ytdl.getInfo(url)
+    console.info(songInfo.formats.map(v=>[v.audioCodec, v.container, v.mimeType, v.itag ]))
+    console.info(songInfo.formats)
+    console.info("112")
     return {
         site: id,
         title: songInfo.videoDetails.title,
